da8e5bc251423e2794b6a2b538285d3826826ec8
ponstomas <t1999pons@gmail.com>
2023-11-22 19:51:14 -0300

    update

--- API Final/app.js
+++ API Final/app.js
@@ -1,52 +1,51 @@
-const express = require("express"); // Importa ExpressJS. Más info de Express en =>https://expressjs.com/es/starter/hello-world.html
+const express = require("express"); 
+const cors = require("cors");
 
-const app = express(); // Crea una instancia de ExpressJS
-
+const app = express();
 const port = 3000;
 
-app.use(express.json()); // Permite que el servidor analice el cuerpo de las peticiones como JSON
-
-const people = require("./json/people.json"); // Importa los datos iniciales (generados en https://www.mockaroo.com/)
+app.use(express.json()); 
+app.use(cors());
 
 app.get("/", (req, res) => {
-  // El primer parámetro SIEMPRE es asociado a la request (petición) y el segundo a la response (respuesta)
   res.send("<h1>Bienvenid@ al servidor</h1>");
 });
 
-app.get("/people", (req, res) => {
-  res.json(people); // Enviamos todo el array
+const cart = require("./json/cart/buy.json"); 
+const sell = require("./json/sell/publish.json");
+const cats = require("./json/cats/cat.json"); 
+const userCart = require("./json/user_cart/25801.json");
+
+
+//Categories
+app.get("/categories", (req, res) => {
+  res.json(cats); 
 });
 
-app.get("/people/:index", (req, res) => {
-  /*La propiedad "params" del request permite acceder a los parámetros de la URL 
-    (importante no confundir con la "query", que serían los parámetros que se colocan 
-    luego del signo "?" en la URL)
-   */
-  res.json(people[req.params.index]); // Enviamos el elemento solicitado por su índice
+app.get("/categories/:index", (req, res) => {
+  res.json(cats[req.params.index])
 });
 
-app.post("/people", (req, res) => {
-  /* La propiedad "body" del request permite acceder a los datos 
-       que se encuentran en el cuerpo de la petición */
-
-  people.push(req.body); // Añadimos un nuevo elemento al array
-
-  res.json(req.body); // Le respondemos al cliente el objeto añadido
+//Categories products
+app.get("/cats_products/:id", (req, res) => {
+  const catsProducts = require(`./json/cats_products/${req.params.id}`)
+  res.json(catsProducts); 
 });
 
-app.put("/people/:index", (req, res) => {
-  /* COMPLETA EL CÓDIGO NECESARIO:
-     Para que se pueda actualizar el objeto asociado al índice indicado en la URL 
-   */
+//Products
+app.get("/products/:id", (req, res) => {
+  const products = require(`./json/products/${req.params.id}`)
+  res.json(products); 
 });
 
-app.delete("/people/:index", (req, res) => {
-  /* COMPLETA EL CÓDIGO NECESARIO:
-     Para que se pueda eliminar el objeto asociado al índice indicado en la URL 
-   */
+//Products comments
+app.get("/products_comments/:id", (req, res) => {
+  const productsComments = require(`./json/products_comments/${req.params.id}`)
+  res.json(productsComments); 
 });
 
-// Esta línea inicia el servidor para que escuche peticiones en el puerto indicado
+
+
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
